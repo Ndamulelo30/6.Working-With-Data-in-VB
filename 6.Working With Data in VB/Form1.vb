14acc2b1eaf21873709a69c6a24d4ac0e3aa2e34
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-20 09:00:50 +0200

    Add project files.

---
+++ 6.Working With Data in VB/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim YourMessage As String
+
+        Dim BirthYear As Integer
+        YourMessage = "Happy Birthday, How old are you now?"
+
+        MessageBox.Show(YourMessage)
+        BirthYear = Val(InputBox("Enter year of birth"))
+        MessageBox.Show("You are now, " & 2024 - BirthYear & " Years old.")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
